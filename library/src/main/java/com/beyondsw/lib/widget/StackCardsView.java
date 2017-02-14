b6430216d58ad588230d5e60baa5fbe9904f70ac
wensefu <wensefu@foxmail.com>
2017-02-14 20:05:30 +0800

    先提交

--- library/src/main/java/com/beyondsw/lib/widget/StackCardsView.java
+++ library/src/main/java/com/beyondsw/lib/widget/StackCardsView.java
@@ -290,6 +290,7 @@ public class StackCardsView extends ViewGroup {
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        Log.d(TAG, "onMeasure");
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         int childCount = getChildCount();
         for (int i = 0; i < childCount; i++) {
