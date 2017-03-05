417ffbe541274811a49356d491d7d18e75966c43
wensefu <wensefu@foxmail.com>
2017-03-05 23:04:29 +0800

    先提交

--- app/src/main/java/com/beyondsw/widget/DemoActivity.java
+++ app/src/main/java/com/beyondsw/widget/DemoActivity.java
@@ -6,8 +6,6 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
 
 /**
  * Created by wensefu on 2017/2/12.
@@ -29,15 +27,6 @@ public class DemoActivity extends AppCompatActivity {
         mCardFragment = new CardFragment();
         mPager = Utils.findViewById(this,R.id.viewpager);
         mPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
-        mPager.setPageTransformer(false, new MyPageTransformer());
-    }
-
-    private static class MyPageTransformer implements ViewPager.PageTransformer {
-
-        @Override
-        public void transformPage(View page, float position) {
-            Log.d(TAG, "transformPage: page=" + page + ",position=" + position);
-        }
     }
 
     private class MyPagerAdapter extends FragmentPagerAdapter {
@@ -48,7 +37,7 @@ public class DemoActivity extends AppCompatActivity {
 
         @Override
         public Fragment getItem(int position) {
-            return position == 0 ? mSettingFragment : mCardFragment;
+            return position == 0 ? mCardFragment : mSettingFragment;
         }
 
         @Override
